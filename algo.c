5588b4daed6c5dc950cf19009c67e54b1c380db2
sudo-abdullah <2022ee19@gmail.com>
2024-07-10 14:33:19 +0500

    Update algo.c

--- algo.c
+++ algo.c
@@ -3,15 +3,13 @@
 #include <math.h>
 
 int main(){
-    int Q = 4,M = 2;
-    int n = 3;  
+    int Q = 4; //Dividend
+    int M = 2; //Divisor
+    int n = 3;  //Number of Bits in dividend
     int A = 0;
-    A = A<<1;
-    if (n==0) {
-
-    }
-    Q = Q<<1;
-    M = M<<1;  
+    A = A<<1; //Left Shift
+    Q = Q<<1; //Left Shift
+    M = M<<1;  //Left Shift
     A = A-M;
     printf("%d %d %d\n",A, Q, M);
     
